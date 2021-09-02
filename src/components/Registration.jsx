f0b675a8147c3f2dd8dc758b8794e3f8f031d021
ivanovsemyon <sivanov.exceedteam@gmail.com>
2021-09-02 12:40:26 +0300

    error messege

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -7,18 +7,57 @@ const Registration = () => {
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
+  const [error, setError] = useState({
+    login: null,
+    password: null,
+    repeatPassword: null,
+  });
 
   const submitForm = async (e) => {
     e.preventDefault();
-      await axios.post("http://localhost:8000/register", {
-        login: login,
-        password: password,
-      }).then(result => {
-        console.log(result)
-      }).catch(error => {
-        error.json().then(e => e.body)
-        console.warn('eeeeeeeeeeeeee', error)
+    if (login.length < 6) {
+      setError({
+        ...error,
+        login: "Длина логина должна быть не менее 6 символов",
       });
+    } else if (!validPassword) {
+      setError({
+        ...error,
+        password:
+          "Длина пароля должна быть не меньше 6 символов, обязательно состоять из латинских символов и содержать число",
+      });
+    } else if (password !== repeatPassword) {
+      setError({
+        ...error,
+        repeatPassword: "Пароли не совпадают",
+      });
+    } else {
+      await axios
+        .post("http://localhost:8000/register", {
+          login: login,
+          password: password,
+          repeatPassword: repeatPassword,
+        })
+        .then((result) => {})
+        .catch((errorBackend) => {
+          if (errorBackend.response.data.login) {
+            setError({
+              ...error,
+              login: errorBackend.response.data.login,
+            });
+          } else if (errorBackend.response.data.password) {
+            setError({
+              ...error,
+              password: errorBackend.response.data.password,
+            });
+          } else if (errorBackend.response.data.repeatPassword) {
+            setError({
+              ...error,
+              repeatPassword: errorBackend.response.data.repeatPassword,
+            });
+          }
+        });
+    }
   };
 
   const passwordRegex = /^(?=.*[\d])[A-Za-z0-9].{5,}$/;
@@ -36,12 +75,13 @@ const Registration = () => {
             placeholder="Login"
             className="form_input"
             value={login}
-            onChange={(e) => setLogin(e.target.value)}
+            onChange={(e) => {
+              setLogin(e.target.value);
+              setError({ ...error, login: null });
+            }}
           />
           <div className="form_error-text">
-            {login.length >= 6 || login === "" || (
-              <p>Логин должен содержать минимум 6 символов</p>
-            )}
+            {error.login && <p>{error.login}</p>}
           </div>
           <label className="form_text">Password:</label>
           <input
@@ -49,18 +89,13 @@ const Registration = () => {
             placeholder="Password"
             className="form_input"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setError({ ...error, password: null });
+            }}
           />
           <div className="form_error-text">
-            {validPassword || password === "" || (
-              <p
-                title="Длина пароля должна быть не меньше 6 символов, обязательно
-                состоять из латинских символов и содержать число"
-              >
-                Длина пароля должна быть не меньше 6 символов, обязательно
-                состоять из латинских символов и содержать число
-              </p>
-            )}
+            {error.password && <p title={error.password}>{error.password}</p>}
           </div>
           <label className="form_text">Repeat password:</label>
           <input
@@ -68,14 +103,22 @@ const Registration = () => {
             placeholder="Password"
             className="form_input"
             value={repeatPassword}
-            onChange={(e) => setRepeatPassword(e.target.value)}
+            onChange={(e) => {
+              setRepeatPassword(e.target.value);
+              setError({ ...error, repeatPassword: null });
+            }}
           />
           <div className="form_error-text">
-            {password === repeatPassword || repeatPassword === "" || (
-              <p>Пароли не совпадают</p>
-            )}
+            {error.repeatPassword && <p>{error.repeatPassword}</p>}
           </div>
-          <button className="form_button">Зарегистрироваться</button>
+          <button
+            className="form_button"
+            disabled={
+              !(!error.login && !error.password && !error.repeatPassword)
+            }
+          >
+            Зарегистрироваться
+          </button>
         </form>
         <a className="link-to-authorization">Авторизоваться</a>
       </div>
